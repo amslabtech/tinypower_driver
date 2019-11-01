b0603a3520643cb7455157a787b950846c67ba10
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-11-01 14:05:55 +0900

    add ros packages

---
+++ knm_tiny_power/src/knm_tiny_kari.cpp
@@ -0,0 +1,44 @@
+
+#include <ros/ros.h>
+#include <knm_tiny_msgs/Velocity.h>
+#include <boost/thread.hpp>
+#include <sensor_msgs/Joy.h>
+
+using namespace std;
+
+
+
+/////////////////////////////////////////////////////////
+//-------------------CallBack!!!!!---------------------//
+/////////////////////////////////////////////////////////
+boost::mutex joy_mutex_;
+knm_tiny_msgs::Velocity cmd_;
+double gain = 0;
+void joycallback(const sensor_msgs::JoyConstPtr& msg)
+{
+	boost::mutex::scoped_lock(joy_mutex_);
+
+	//gain = (msg->axes[2]+1.0) * (1-msg->buttons[8]*0.5) * (1+msg->buttons[10]*0.6);
+	gain = (1-msg->buttons[8]*0.5) * (1+msg->buttons[10]*0.6);
+	cmd_.op_linear=msg->axes[1]*0.8*gain;
+	cmd_.op_angular=-msg->axes[0]*1.2*gain;
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "knm_tiny_kari");
+
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("/joy", 10, joycallback);
+	ros::Publisher pub = n.advertise<knm_tiny_msgs::Velocity>("tinypower/command_velocity", 10);
+	
+	ros::Rate r(20);
+	while(ros::ok()){
+		pub.publish(cmd_);
+		
+		ros::spinOnce();
+		r.sleep();
+		
+	}
+}
+
